3b77c494e884fd41c87a472a9bbd6db6b35ccb7d
97tuna <ldh2033@soongsil.ac.kr>
2021-10-25 00:19:52 +0900

    [UPD] Release

--- firebase.py
+++ firebase.py
@@ -1,6 +1,6 @@
 from flask import Flask, json, request, jsonify
 from flask_restful import Resource, Api
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from OpenSSL import SSL
 import datetime as dt
 import logging, ssl
